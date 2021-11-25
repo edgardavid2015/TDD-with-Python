feb4b30d145463e6fa922c00d3d95ef5434c9d97
Edgar Guaman <edgar.guaman@ioet.com>
2021-11-25 01:15:54 -0500

    End of password exercise

--- password/password.py
+++ password/password.py
@@ -1,27 +1,22 @@
 def password_validator(password):
+    error_list = []
     if len(password) < 8:
-        return 'Password length must be greater than 8 characters'
+        error_list.append('Password length must be greater than 8 characters')
+
     total_numbers = [digit for digit in password if digit.isnumeric()]
     if len(total_numbers) < 2:
-        return 'Password must have at least 2 numbers'
+        error_list.append('Password must have at least 2 numbers')
+
     total_letters = [char for char in password if char.isupper()]
     if len(total_letters) < 1:
-        return 'Password must have at least one uppercase letter'
-    return 'Password accepted'
+        error_list.append('Password must have at least 1 uppercase letter')
 
+    total_esp_chars = [char for char in password if not password.isalnum() and not password.isspace()]
+    if len(total_esp_chars) < 1:
+        error_list.append('Password must have at least 1 special character')
 
-def verify_char(char):
-    if char.isupper():
-        return print('letra')
+    if len(error_list) > 0:
+        string_errors = '\n'.join(error_list)
+        return string_errors
     else:
-        return print('numero')
-
-
-def main():
-    verify_char('A')
-    verify_char('a')
-    verify_char('1')
-    password_validator('1234AbcD')
-
-
-main()
+        return 'Password accepted'
--- password/password_test.py
+++ password/password_test.py
@@ -1,24 +1,40 @@
-# hacer una funcion que valide un password:
-# 1 largo de al menos 8 caracteres, si no devuelve este mensaje: El password por lo menos debe tener 8 char
-# 2 que por lo menos contenga 2 numeros. sino que de devuelva un mensaje diciendote que debe tener 2 numeros
-# 3 si el passwor tiene mas de 1 error que te devuelva todos los mensajes de error separados por un \n
-# 4 que el pass tenga por lo menos una letra en mayuscula, sino mensaje de error descriptivo
-# 5 que el pass tenga por lo menos un caracter especial, sino mensaje de error.
+# Apply the TDD methodology to create a validation function for a password:
+# 1.- The length must be at least eight characters, otherwise it returns a descriptive error message.
+# 2.- The password must have at least two numbers, otherwise it returns a descriptive error message.
+# 3.- The password must have at least one capital letter, otherwise it returns a descriptive error message
+# 4.- The password must have at least one special character, otherwise it returns a descriptive error message.
+# 5.- If the password has more than one error message, the function must return all these messages separated by \n.
 
 import pytest
 from password import password_validator
 
 
-@pytest.mark.parametrize('invalid_length_passwords', ['1234', '12345', '1234567'])
+@pytest.mark.parametrize('invalid_length_passwords', ['1234A*', '12345B*', '12346C*'])
 def test_show_error_message_when_password_length_is_less_than_eight_char(invalid_length_passwords):
     assert password_validator(invalid_length_passwords) == 'Password length must be greater than 8 characters'
 
 
-@pytest.mark.parametrize('invalid_numbers_in_password', ['1Aabbccdd', 'tuvWxyz1', '1passworD'])
+@pytest.mark.parametrize('invalid_numbers_in_password', ['1Aabbccd-', 'tuvWxy-1', '1-passworD'])
 def test_show_error_message_when_password_has_less_than_two_numbers(invalid_numbers_in_password):
     assert password_validator(invalid_numbers_in_password) == 'Password must have at least 2 numbers'
 
 
-@pytest.mark.parametrize('invalid_uppercase_letters_passwords', ['01aabbccdd', 'mnopq1234', 'p4ssw0rd'])
+@pytest.mark.parametrize('invalid_uppercase_letters_passwords', ['01aa+ccdd', 'mno+pq1234', 'p4ss+w0rd'])
 def test_show_error_message_when_password_has_less_than_one_uppercase_letter(invalid_uppercase_letters_passwords):
-    assert password_validator(invalid_uppercase_letters_passwords) == 'Password must have at least one uppercase letter'
+    assert password_validator(invalid_uppercase_letters_passwords) == 'Password must have at least 1 uppercase letter'
+
+
+@pytest.mark.parametrize('invalid_special_characters_passwords', ['01Aabbcc', 'HIJKL1234', 'USERn4m3'])
+def test_show_error_message_when_password_has_less_than_one_special_character(invalid_special_characters_passwords):
+    assert password_validator(invalid_special_characters_passwords) == 'Password must have at least 1 special character'
+
+
+@pytest.mark.parametrize('invalid_length_and_numbers_in_password', ['1Aabbc-', 'Wxy-1', '1-pasrD'])
+def test_show_two_error_messages_related_to_length_and_numbers_in_password(invalid_length_and_numbers_in_password):
+    assert password_validator(invalid_length_and_numbers_in_password) == \
+           'Password length must be greater than 8 characters' + '\n' + 'Password must have at least 2 numbers'
+
+
+@pytest.mark.parametrize('valid_passwords', ['0123ABC**', 'T1CT4CT0E+', '-F1zzB0zz-'])
+def test_show_error_message_when_password_has_less_than_one_special_character(valid_passwords):
+    assert password_validator(valid_passwords) == 'Password accepted'
