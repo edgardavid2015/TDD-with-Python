80d7e797e65c7e715bfbbd39e5860d5b5c750e02
Edgar Guaman <edgar.guaman@ioet.com>
2021-11-26 12:09:46 -0500

    Changes in params names and tests

--- password/password.py
+++ password/password.py
@@ -1,19 +1,19 @@
 def password_validator(password):
     error_list = []
     if len(password) < 8:
-        error_list.append('Password length must be greater than 8 characters')
+        error_list.append('Password length must be greater than eight characters')
 
     total_numbers = [digit for digit in password if digit.isnumeric()]
     if len(total_numbers) < 2:
-        error_list.append('Password must have at least 2 numbers')
+        error_list.append('Password must have at least two numbers')
 
     total_letters = [char for char in password if char.isupper()]
     if len(total_letters) < 1:
-        error_list.append('Password must have at least 1 uppercase letter')
+        error_list.append('Password must have at least one uppercase letter')
 
     total_esp_chars = [char for char in password if not password.isalnum() and not password.isspace()]
     if len(total_esp_chars) < 1:
-        error_list.append('Password must have at least 1 special character')
+        error_list.append('Password must have at least one special character')
 
     if len(error_list) > 0:
         string_errors = '\n'.join(error_list)
--- password/password_test.py
+++ password/password_test.py
@@ -9,32 +9,35 @@ import pytest
 from password import password_validator
 
 
-@pytest.mark.parametrize('invalid_length_passwords', ['1234A*', '12345B*', '12346C*'])
-def test_show_error_message_when_password_length_is_less_than_eight_char(invalid_length_passwords):
-    assert password_validator(invalid_length_passwords) == 'Password length must be greater than 8 characters'
+@pytest.mark.parametrize('test_password', ['1234A*', '12345B*', '12346C*'])
+def test_show_error_message_when_password_length_is_less_than_eight_char(test_password):
+    assert password_validator(test_password) == 'Password length must be greater than eight characters'
 
 
-@pytest.mark.parametrize('invalid_numbers_in_password', ['1Aabbccd-', 'tuvWxy-1', '1-passworD'])
-def test_show_error_message_when_password_has_less_than_two_numbers(invalid_numbers_in_password):
-    assert password_validator(invalid_numbers_in_password) == 'Password must have at least 2 numbers'
+@pytest.mark.parametrize('test_password', ['1Aabbccd-', 'tuvWxy-1', '1-passworD'])
+def test_show_error_message_when_password_has_less_than_two_numbers(test_password):
+    assert password_validator(test_password) == 'Password must have at least two numbers'
 
 
-@pytest.mark.parametrize('invalid_uppercase_letters_passwords', ['01aa+ccdd', 'mno+pq1234', 'p4ss+w0rd'])
-def test_show_error_message_when_password_has_less_than_one_uppercase_letter(invalid_uppercase_letters_passwords):
-    assert password_validator(invalid_uppercase_letters_passwords) == 'Password must have at least 1 uppercase letter'
+@pytest.mark.parametrize('test_password', ['01aa+ccdd', 'mno+pq1234', 'p4ss+w0rd'])
+def test_show_error_message_when_password_has_less_than_one_uppercase_letter(test_password):
+    assert password_validator(test_password) == 'Password must have at least one uppercase letter'
 
 
-@pytest.mark.parametrize('invalid_special_characters_passwords', ['01Aabbcc', 'HIJKL1234', 'USERn4m3'])
-def test_show_error_message_when_password_has_less_than_one_special_character(invalid_special_characters_passwords):
-    assert password_validator(invalid_special_characters_passwords) == 'Password must have at least 1 special character'
+@pytest.mark.parametrize('test_password', ['01Aabbcc', 'HIJKL1234', 'USERn4m3'])
+def test_show_error_message_when_password_has_less_than_one_special_character(test_password):
+    assert password_validator(test_password) == 'Password must have at least one special character'
 
 
-@pytest.mark.parametrize('invalid_length_and_numbers_in_password', ['1Aabbc-', 'Wxy-1', '1-pasrD'])
-def test_show_two_error_messages_related_to_length_and_numbers_in_password(invalid_length_and_numbers_in_password):
-    assert password_validator(invalid_length_and_numbers_in_password) == \
-           'Password length must be greater than 8 characters' + '\n' + 'Password must have at least 2 numbers'
+@pytest.mark.parametrize('test_password', ['1abbc', 'wxy7', '9pasr'])
+def test_show_all_error_messages_separated_by_new_line_character(test_password):
+    assert password_validator(test_password) == \
+           'Password length must be greater than eight characters' + '\n' + \
+           'Password must have at least two numbers' + '\n' + \
+           'Password must have at least one uppercase letter' + '\n' + \
+           'Password must have at least one special character'
 
 
-@pytest.mark.parametrize('valid_passwords', ['0123ABC**', 'T1CT4CT0E+', '-F1zzB0zz-'])
-def test_show_error_message_when_password_has_less_than_one_special_character(valid_passwords):
-    assert password_validator(valid_passwords) == 'Password accepted'
+@pytest.mark.parametrize('test_password', ['0123ABC**', 'T1CT4CT0E+', '-F1zzB0zz-'])
+def test_show_accepted_message_when_password_is_correct(test_password):
+    assert password_validator(test_password) == 'Password accepted'
